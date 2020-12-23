8061a43e349188630c1e4508e8bde24848541658
Siddhant Mittal <sidmit2001@gmail.com>
2020-12-23 23:06:59 +0530

    final

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,15 +1,35 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { jsx } from "theme-ui";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default () => {
-  const router = useRouter()
-  const { id }= router.query
+const Note = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
   return (
-    <div sx={{variant: 'containers.page'}}>
+    <div sx={{ variant: "containers.page" }}>
       <h1>Note: {id} </h1>
     </div>
-  )
+  );
+};
+
+export default Note;
+
+export async function getServerSideProps({ params, req, res }) {
+  const response = await fetch(`http://localhost:3000/api/notes/${params.id}`);
+
+  if (!response.ok) {
+    res.writeHead(302, { Location: "/notes" });
+    res.end();
+    return { props: {} };
+  }
+
+  const { data } = await response.json();
+
+  if (data) {
+    return {
+      props: { note: data },
+    };
+  }
 }
